d0a2ce62532d113998fe8e2d5473ab1429661a2d
Egidijus Adzgauskas <eadzgauskas@gmail.com>
2021-09-05 23:56:52 +0300

    mano bandymas

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace gitHub_mokymai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sarasas = new List<string>() { "Geltona", "Zalia", "Raudona", "Lietuva" };
+            for (int i = 0; i < sarasas.Count; i++)
+            {
+                Console.WriteLine(sarasas[i]);
+            }
+
+        }
+    }
+}
